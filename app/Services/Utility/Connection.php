ad5bdfd6f58dfe144c322642b038fbf34a69b02d
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-02-27 18:42:39 -0700

    added code to check for the connection

--- app/Services/Utility/Connection.php
+++ app/Services/Utility/Connection.php
@@ -5,6 +5,7 @@
  * Description: Connections holds the database information
  */
 namespace App\Services\Utility;
+use Illuminate\Support\Facades\Log;
 use PDO;
 class Connection
 {
@@ -25,11 +26,18 @@ class Connection
         $this->un = config("database.connections.mysql.username");
         $this->pw = config("database.connections.mysql.password");
         $this->db = config("database.connections.mysql.database");
+
         // A new PDO is opened using information from the config file.
         $conn = new PDO("mysql:host=$this->sn;dbname=$this->db", $this->un, $this->pw);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        if(!conn){
+           Log::error("Connection Error: ", array("message" => $e->getMessage())); 
+        }
+        else {
         // The connection is returned.
         return $conn;
+        }
     }
 }
 ?>
