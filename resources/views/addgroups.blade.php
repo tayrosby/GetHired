b81c703216ec01aec0b06f0b398bcde97af4f197
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-03-02 11:12:25 -0700

    input hard coded user id for testing

--- resources/views/addgroups.blade.php
+++ resources/views/addgroups.blade.php
@@ -11,7 +11,7 @@
 		<hr>
 		<table>
 		<tr>
-			<td><input type="hidden" name = "userID" value = "9"><td>
+			<td><input type="hidden" name = "userID" value = "11"><td>
 			<tr>
 			<tr>
 				<td>Group Name: </td>
