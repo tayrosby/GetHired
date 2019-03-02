7adea65975e7f3ea92b324bca5e82297362df9bf
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-03-02 13:40:27 -0700

    added leave group button

--- resources/views/allgroups.blade.php
+++ resources/views/allgroups.blade.php
@@ -31,14 +31,24 @@ $group = $gbs->findAllGroups();
               <td> {{ $group[$i]['INTEREST'] }} </td>
               <td> {{ $group[$i]['GROUP_DESCRIPTION'] }} </td>
 
-                          <form id="joinGroup{{$group[$i]['ID']}}" action="addmember" method="POST">
+              <form id="joinGroup{{$group[$i]['ID']}}" action="addmember" method="POST">
 						<input type="hidden" name="_token" value="{{csrf_token()}}"/>
 						<input type="hidden" name="groupID" value="{{$group[$i]['ID']}}"/>
 						<input type="hidden" name="userID" value="{{$group[$i]['USERS_ID']}}"/>
 			</form>
 					
 					<td><input form="joinGroup{{$group[$i]['ID']}}" class="btn" type="submit" value="Join Group"/></td>
+					
                         </tr>
          @endfor
+         
+         <form id="leaveGroup{{$group[$i]['ID']}}" action="deletemember" method="POST">
+						<input type="hidden" name="_token" value="{{csrf_token()}}"/>
+						<input type="hidden" name="groupID" value="{{$group[$i]['ID']}}"/>
+						<input type="hidden" name="userID" value="{{$group[$i]['USERS_ID']}}"/>
+			</form>
+			
+			<td><input form="leaveGroup{{$group[$i]['ID']}}" class="btn" type="submit" value="Leave Group"/></td>
    </tbody>
 </table>
+@endsection
