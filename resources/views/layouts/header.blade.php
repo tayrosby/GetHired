a6bfd9f8ecf609a4a91e96ea63ec0f4281959bb7
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-03-11 15:41:01 -0700

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,7 +13,7 @@
         <a class="nav-link" href="joppage">Jobs</a>
       </li>
        <li class="nav-item">
-        <a class="nav-link" href="jobsearch">Job Search</a>
+        <a class="nav-link" href="searchjobs">Job Search</a>
       </li>
        <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
