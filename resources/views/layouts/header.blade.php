e86ec83f28e33ca221fedee75c1894b25bf1761f
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-03-11 15:38:00 -0700

    added job search link

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="homepage"><img src="/resources/images/GetHiredV2.1.jpg" alt = "g in a circle" height = "25" width = "25"></a>
+  <a class="navbar-brand" href="homepage"><img src="/GetHired/resources/images/GetHiredV2.1.jpg" alt = "g in a circle" height = "25" width = "25"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -12,12 +12,15 @@
       <li class="nav-item">
         <a class="nav-link" href="joppage">Jobs</a>
       </li>
-      <li class="nav-item dropdown">
+       <li class="nav-item">
+        <a class="nav-link" href="jobsearch">Job Search</a>
+      </li>
+       <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Groups
         </a>
          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="groups">Groups</a>
+          <a class="dropdown-item" href="allgroups">Groups</a>
           <a class="dropdown-item" href="addgroups">Add Group</a>
           <a class="dropdown-item" href="managegroups">Manage Group</a>
         </div>
@@ -25,21 +28,22 @@
       <li class="nav-item">
         <a class="nav-link" href="profile">Profile</a>
       </li>
-        @if(session('userID') == NULL)
       <li class="nav-item">
+       @if(session('user') == NULL) 
         <a class="nav-link" href="login">Login</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="register">Sign up</a>
       </li>
-      @else
        <li class="nav-item">
+       @else
         <a class="nav-link" href="logout">Log Out</a>
+        
       </li>
-      @endif
       
-      @if(session('role') == "1")
       <li class="nav-item dropdown">
+      @if(session()->has('user')) 
+      		@if(session['role'] == '1')
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Admin
         </a>
@@ -48,8 +52,10 @@
           <a class="dropdown-item" href="addjob">Add Job</a>
           <a class="dropdown-item" href="managejob">Manage Job</a>
         </div>
+        	@endif
+        @endif
+        @endif
       </li>
-      @endif
     </ul>
   </div>
 </nav>
