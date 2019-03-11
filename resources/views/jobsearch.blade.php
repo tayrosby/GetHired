71e71f8c37daf029a0de3ba73d7d940e3ec98ad3
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-03-11 15:38:31 -0700

    Create jobsearch.blade.php

---
+++ resources/views/jobsearch.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+@extends('layouts.maintemplate')
+@section('title', 'Get Hired! | Job Search')
+
+@section('content')
+
+
+<h1 class = "h1">Get Hired!</h1>
+
+<div class="container-fluid">
+<form class = "loginform" action="searchDescription" method="GET">
+<input type = "hidden" name ="_token" value = "<?php echo csrf_token()?>"/>
+<h2>Search</h2>
+<hr>
+<!--  allows for user input and passes the info to the processor file -->
+
+<div class="form-group">
+<input type="text" placeholder= "Search" name="descriptionSearch" maxlength="10">{{ $errors->first('descriptionSearch') }}<br>
+</div>
+  
+ <input type = "submit" value = "Search"/><br>
+ <hr>
+ </form>
+</div> 
+@endsection
