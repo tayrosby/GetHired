597355d9e7f8721a9d1a51e73db55f2646b77a12
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-03-11 23:16:48 -0700

    Update jobdetails.blade.php

--- resources/views/jobdetails.blade.php
+++ resources/views/jobdetails.blade.php
@@ -2,6 +2,8 @@
 @section('title', 'Get Hired! | Job Details')
 
 @section('content')
+
+@for($x = 0; $x < count($jobs); $x++)
 	<div class="container-fluid">
 	<form class = "loginform" action="jobApp" method="POST">
 		<input type="hidden" name="_token" value="<?php echo csrf_token() ?>"/>
@@ -12,32 +14,32 @@
 		<table>
 			<tr>
 				<td>Job Position: </td>
-				<td><input type="text" name="position"/>{{ $errors->first('position') }}</td>
+				<td><input type="text" name="position" value="{{ $jobs[$x]['POSITION'] }}"/>{{ $errors->first('position') }}</td>
 			</tr>
 			
 			<tr>
 				<td>Company: </td>
-				<td><input type="text" name="company"/>{{ $errors->first('company') }}</td>
+				<td><input type="text" name="company" value="{{ $jobs[$x]['COMPANY'] }}"/>{{ $errors->first('company') }}</td>
 			</tr>
 			
 			<tr>
 				<td>Location: </td>
-				<td><input type="text" name="location"/>{{ $errors->first('location') }}</td>
+				<td><input type="text" name="location" value="{{ $jobs[$x]['LOCATION'] }}"/>{{ $errors->first('location') }}</td>
 			</tr>
 			
 			<tr>
 				<td>Requirements: </td>
-				<td><input type="text" name="requirements"/>{{ $errors->first('requirements') }}</td>
+				<td><input type="text" name="requirements" value="{{ $jobs[$x]['REQUIREMENTS'] }}"/>{{ $errors->first('requirements') }}</td>
 			</tr>
 			
 			<tr>
 				<td>Level: </td>
-				<td><input type="text" name="level"/>{{ $errors->first('level') }}</td>
+				<td><input type="text" name="level" value="{{ $jobs[$x]['LEVEL'] }}"/>{{ $errors->first('level') }}</td>
 			</tr>
 			
 			<tr>
 				<td>Description: </td>
-				<td><input type="text" name="description"/>{{ $errors->first('description') }}</td>
+				<td><input type="text" name="description" value="{{ $jobs[$x]['DESCRIPTION'] }}"/>{{ $errors->first('description') }}</td>
 			</tr>
 			
 			<tr>
@@ -49,4 +51,5 @@
 		</div>
 	</form>
 	</div> 
+	@endfor
 @endsection
