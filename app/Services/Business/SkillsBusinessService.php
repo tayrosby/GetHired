bbeba7ef301163966e51ed268632d5ae263fd995
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-03-14 14:10:32 -0700

    added comments

--- app/Services/Business/SkillsBusinessService.php
+++ app/Services/Business/SkillsBusinessService.php
@@ -9,57 +9,103 @@ use App\Services\Data\SkillsDataService;
 use App\Services\Utility\Connection;
 class SkillsBusinessService
 {
-    //passes the model to the data service
+    /**
+     * passes the model to the data service
+     * @param $skills
+     * @return boolean
+     */
     public function editSkills($skills)
     {
+        //creates a connection
         $db = new Connection();
         $conn = $db->open();
         
+        //calls the data service
         $service = new SkillsDataService($conn);
+        
+        //calls the edit method in the data service
         $editSuccess = $service->editSkill($skills);
         
+        //closes the connection
         $conn = null;
+        
+        //if successful return true, else return false
         if ($editSuccess == 1) { return true; }
         else { return false; }
     }
     
+    /**
+     * passes the model to the data service
+     * @param $skills
+     * @return boolean
+     */
     public function addSkills($skills)
     {
+        //creates a connection
         $db = new Connection();
         $conn = $db->open();
         
+        //calls the data service
         $service = new SkillsDataService($conn);
+        
+        //calls the create method in the data service
         $createSuccess = $service->createSkill($skills);
         
+        //closes the connection
         $conn = null;
+        
+        //if successful return true, else return false
         if ($createSuccess == 1) { return true; }
         else { return false; }
     }
     
+    /**
+     * finds all the skills in the data service
+     * @return array
+     */
     public function findAllSkills()
     {
+        //creates a connection
         $db = new Connection();
         $conn = $db->open();
         
+        //creates an array of skills
         $skills = Array();
         
+        //calls the data service
         $service = new SkillsDataService($conn);
+        
+        //calls the find all method in the data service
         $skills = $service->findAll();
         
+        //closes the connection
         $conn = null;
 
+        //returns array
         return $skills;
     }
     
+    /**
+     * passes the model to the data service
+     * @param $skills
+     * @return boolean
+     */
     public function deleteSkills($skills)
     {
+        //creates a connection
         $db = new Connection();
         $conn = $db->open();
         
+        //calls the data service
         $service = new SkillsDataService($conn);
+        
+        //calls the delete method in the data service
         $deleteSuccess = $service->deleteSkill($skills);
         
+        //closes the connection
         $conn = null;
+        
+        //if successful return true, else return false
         if ($deleteSuccess == 1) { return true; }
         else { return false; }
     }
