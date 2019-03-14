02d2e91c8116fcbdef68996f72a3ff9731087e63
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-03-13 20:30:03 -0700

    added comments

--- app/Services/Utility/DatabaseException.php
+++ app/Services/Utility/DatabaseException.php
@@ -8,8 +8,12 @@ namespace App\Services\Utility;
 use Exception;
 class DatabaseException extends Exception
 {
-    
-    // Non-Default constructor
+    /**
+     * Non-Default constructor
+     * @param $message
+     * @param number $code
+     * @param Exception $previous
+     */
     public function __construct($message, $code = 0, Exception $previous = null)
     {
         // Call super class
