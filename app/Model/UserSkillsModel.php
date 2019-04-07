0b49dbb20bb6ba6e04aa22190c83f19da7832427
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-04-07 11:13:19 -0700

    Update UserSkillsModel.php

--- app/Model/UserSkillsModel.php
+++ app/Model/UserSkillsModel.php
@@ -6,7 +6,7 @@
  */
 namespace App\Model;
 
-class UserSkillsModel
+class UserSkillsModel implements \JsonSerializable
 {
     //attributes
     private $id;
@@ -32,6 +32,11 @@ class UserSkillsModel
     public function __get($property)
     {
         if (property_exists($this, $property)) { return $this->$property; }
+    }
+    
+    public function jsonSerialize()
+    {
+        return get_object_vars($this);
     }
 }
 ?>
