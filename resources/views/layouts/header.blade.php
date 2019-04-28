570492a8c11bdaa36d0c30f533e161969cb16adb
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-04-27 21:44:02 -0700

    deleted homepage

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="homepage"><img src="/resources/images/GetHiredV2.1.jpg" alt = "g in a circle" height = "25" width = "25"></a>
+  <a class="navbar-brand" href="profilepage"><img src="/resources/images/GetHiredV2.1.jpg" alt = "g in a circle" height = "25" width = "25"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="homepage">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item">
         <a class="nav-link" href="joppage">Jobs</a>
       </li>
