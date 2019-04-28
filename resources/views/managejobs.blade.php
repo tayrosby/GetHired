9b6d17e6ff4aaf43c86e3563c75b391f7bcf89db
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-04-27 21:02:11 -0700

    Update managejobs.blade.php

--- resources/views/managejobs.blade.php
+++ resources/views/managejobs.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Get Hired! | Manage Jobs')
 
 @section('content')
-<div class = "loginform">
+<div class = "registerform">
 	<table>
 	
 		<thead>
@@ -21,14 +21,14 @@
 		<tbody>
 			 @for ($i = 0; $i < count($jobs); $i++)
           <tr>
-              <td> {{ $jobs[$i]['POSITION'] }} </td>
-              <td> {{ $jobs[$i]['COMPANY'] }} </td>
-              <td> {{ $jobs[$i]['LOCATION'] }} </td>
-              <td> {{ $jobs[$i]['REQUIREMENTS'] }} </td>
-              <td> {{ $jobs[$i]['LEVEL'] }} </td>
-              <td> {{ $jobs[$i]['DESCRIPTION'] }} </td>
-					<td> <!-- Button to open the modal -->
-<button onclick="document.getElementById('id01').style.display='block'">Edit</button>
+              <td> {{ $job[$i]['POSITION'] }} </td>
+              <td> {{ $job[$i]['COMPANY'] }} </td>
+              <td> {{ $job[$i]['LOCATION'] }} </td>
+              <td> {{ $job[$i]['REQUIREMENTS'] }} </td>
+              <td> {{ $job[$i]['LEVEL'] }} </td>
+              <td> {{ $job[$i]['DESCRIPTION'] }} </td>
+			  <td> <!-- Button to open the modal -->
+                   <button onclick="document.getElementById('id01').style.display='block'">Edit</button>
 
 <!-- The Modal (contains the Sign Up form) -->
 <div id="id01" class="modal">
@@ -37,26 +37,26 @@
   <input type="hidden" name="_token" value="<?php echo csrf_token() ?>" />
     <div class="container">
       <h1>Edit Job</h1>
-      <input type="hidden" name = "id" value = "{{ $jobs[$i]['ID'] }}">
+      <input type="hidden" name = "id" value = "{{ $job[$i]['ID'] }}">
       <hr>
       
       <label for="position"><b>Position</b></label>
-      <input type="text" placeholder="Enter Position" name="position" value="{{ $jobs[$i]['POSITION'] }}">{{ $errors->first('position') }}
+      <input type="text" placeholder="Enter Position" name="position" value="{{ $job[$i]['POSITION'] }}">{{ $errors->first('position') }}
       
        <label for="position"><b>Company</b></label>
-      <input type="text" placeholder="Enter Position" name="company" value="{{ $jobs[$i]['COMPANY'] }}">{{ $errors->first('company') }}
+      <input type="text" placeholder="Enter Position" name="company" value="{{ $job[$i]['COMPANY'] }}">{{ $errors->first('company') }}
       
        <label for="position"><b>Location</b></label>
-      <input type="text" placeholder="Enter Position" name="location" value="{{ $jobs[$i]['LOCATION'] }}">{{ $errors->first('location') }}   
+      <input type="text" placeholder="Enter Position" name="location" value="{{ $job[$i]['LOCATION'] }}">{{ $errors->first('location') }}   
       
        <label for="position"><b>Requirements</b></label>
-      <input type="text" placeholder="Enter Position" name="requirements" value="{{ $jobs[$i]['REQUIREMENTS'] }}"> {{ $errors->first('requirements') }}   
+      <input type="text" placeholder="Enter Position" name="requirements" value="{{ $job[$i]['REQUIREMENTS'] }}"> {{ $errors->first('requirements') }}   
       
        <label for="position"><b>Level</b></label>
-      <input type="text" placeholder="Enter Position" name="level" value="{{ $jobs[$i]['LEVEL'] }}"> {{ $errors->first('level') }}   
+      <input type="text" placeholder="Enter Position" name="level" value="{{ $job[$i]['LEVEL'] }}"> {{ $errors->first('level') }}   
       
        <label for="position"><b>Description</b></label>
-      <input type="text" placeholder="Enter Position" name="description" value="{{ $jobs[$i]['DESCRIPTION'] }}">{{ $errors->first('description') }}
+      <input type="text" placeholder="Enter Position" name="description" value="{{ $job[$i]['DESCRIPTION'] }}">{{ $errors->first('description') }}
 
       <div class="clearfix">
         <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
@@ -68,7 +68,7 @@
 </td>
 
 <td> <!-- Button to open the modal -->
-<button onclick="document.getElementById('id02').style.display='block'">Delete</button>
+     <button onclick="document.getElementById('id02').style.display='block'">Delete</button>
 
 <!-- The Modal (contains the Sign Up form) -->
 <div id="id02" class="modal">
@@ -77,7 +77,7 @@
   <input type="hidden" name="_token" value="<?php echo csrf_token() ?>" />
     <div class="container">
       <h1>Delete Job</h1>
-      <input type="hidden" name = "ID" value = "{{ $jobs[$i]['ID'] }}">
+      <input type="hidden" name = "ID" value = "{{ $job[$i]['ID'] }}">
       <hr>   
  	<p>Are you sure you want to delete?</p>
       <div class="clearfix">
@@ -86,11 +86,11 @@
       </div>
     </div>
   </form>
+  </div>
 </div>
 </td>
 
-					
-				</tr>
+</tr>
  @endfor
 		</tbody>
 	</table>
@@ -105,6 +105,4 @@ window.onclick = function(event) {
   }
 }
 </script>
-
-</div>
 @endsection
