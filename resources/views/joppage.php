9bb9dc5abdd9553d9d166ff2d915226335e61cfa
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-04-27 20:43:20 -0700

    Update joppage.php

--- resources/views/joppage.php
+++ resources/views/joppage.php
@@ -3,17 +3,39 @@
 
 @section('content')
 
-			@foreach($jobs as $job)
+@for($x = 0; $x < count($jobs); $x++)
 
-					<!-- ForEach Loop -->
-					@foreach($job as $value)
-						<form class ="registerform" >
-						{{ $value }}
-						</form>
-					@endforeach
+<div class="container-fluid">
+<form class = "registerform">
+		
+<input type="hidden" name="_token" value="<?php echo csrf_token() ?>"/>
 					
+ <div class="form-row">
+    <div class="form-group col-md-6">
+      <input type="text" name="position" value="{{ $jobs[$x]['POSITION'] }}">
+    </div>
+    <div class="form-group col-md-6">
+      <input type="text" name="company" value="{{ $jobs[$x]['COMPANY'] }}">
+    </div>
+  </div>
+  <div class="form-group">
+    <input type="text" name="location" value="{{ $jobs[$x]['LOCATION'] }}">
+  </div>
+  <div class="form-group">
+    <input type="text" name="description" value="{{ $jobs[$x]['DESCRIPTION'] }}">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <input  type="text" name="level" value="{{ $jobs[$x]['LEVEL'] }}">
+    </div>
+    <div class="form-group col-md-2">
+      <label>Requirements</label>
+      <input type="text" name="requirements" value="{{ $jobs[$x]['REQUIREMENTS'] }}">
+    </div>
+    </div>
+</form>
 
-			@endforeach
+</div>
 
-
+@endfor
 @endsection
