52295493f05ca2c47da8490f5e292556acddd41c
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-04-28 00:49:38 -0700

    added logging

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -6,16 +6,25 @@
  * the business side of the program.
  */
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Support\Facades\Log;
 use Exception;
 use App\Model\GroupModel;
 use App\Services\Business\GroupBusinessService;
+use App\Services\Utility\ILoggerService;
 
 class GroupController extends Controller
 {
+    protected $logger;
+    
+    /**
+     * 
+     * @param ILoggerService $logger
+     */
+    public function __construct(ILoggerService $logger){
+        $this->logger = $logger;
+    }
+    
     /**
      * adds the group information to the database
      * @param Request $request
@@ -25,6 +34,7 @@ class GroupController extends Controller
     public function addGroup(Request $request){
         
         try {
+            $this->logger->info("Entering GroupController.addGroup()");
             //validate the form data(will redirect back to login view if errors)
             $this->validateForm($request);
             
@@ -46,21 +56,23 @@ class GroupController extends Controller
             //fail or succeed return to all groups
             if($success)
             {
+                $this->logger->info("Entering GroupController.addGroup() with success");
                 return view("allgroups");
             }
             else
             {
+                $this->logger->info("Entering GroupController.addGroup() with failure");
                 return view("addgroups");
             }
         }
-                catch (ValidationException $e1){
-                    //catch and rethrow the data validation exceptions (so we can catch all others in our next exception catch block)
-                    throw $e1;
-                }
+        catch (ValidationException $e1){
+            //catch and rethrow the data validation exceptions (so we can catch all others in our next exception catch block)
+            throw $e1;
+        }
         catch (Exception $e){
             //Best practice: catch all exceptions, log the exception, and display the common error page (or use global exception handling
             //log the exception and display exception view
-            Log::error("Exception: ", array("message" => $e->getMessage()));
+            $this->logger->error("Exception: ", array("message" => $e->getMessage()));
             $data = ['errorMSG' => $e->getMessage()];
             return view('exception')->with($data);
         }
@@ -75,6 +87,7 @@ class GroupController extends Controller
     public function editGroups(Request $request)
     {
         try {
+            $this->logger->info("Entering GroupController.editGroups()");
             //validate the form data(will redirect back to login view if errors)
             $this->validateForm($request);
             
@@ -97,21 +110,23 @@ class GroupController extends Controller
             //fail or succeed return to all groups
             if($success)
             {
+                $this->logger->info("Entering GroupController.editGroups() with success");
                 return view("managegroups");
             }
             else
             {
+                $this->logger->info("Entering GroupController.editGroups() with failure");
                 return view("managegroups");
             }
         }
-                catch (ValidationException $e1){
-                    //catch and rethrow the data validation exceptions (so we can catch all others in our next exception catch block)
-                    throw $e1;
-                }
+        catch (ValidationException $e1){
+            //catch and rethrow the data validation exceptions (so we can catch all others in our next exception catch block)
+            throw $e1;
+        }
         catch (Exception $e){
             //Best practice: catch all exceptions, log the exception, and display the common error page (or use global exception handling
             //log the exception and display exception view
-            Log::error("Exception: ", array("message" => $e->getMessage()));
+            $this->logger->error("Exception: ", array("message" => $e->getMessage()));
             $data = ['errorMSG' => $e->getMessage()];
             return view('exception')->with($data);
         }
@@ -125,6 +140,7 @@ class GroupController extends Controller
     public function deleteGroups(Request $request)
     {
         try {
+            $this->logger->info("Entering GroupController.deleteGroups()");
             //takes information from the user
             $id = $request->input('id');
             
@@ -137,17 +153,19 @@ class GroupController extends Controller
             //fail or succeed return to all groups
             if($success)
             {
+                $this->logger->info("Entering GroupController.deleteGroups() with success");
                 return view("managegroups");
             }
             else
             {
+                $this->logger->info("Entering GroupController.deleteGroups() with failure");
                 return view("managegroups");
             }
         }
         catch (Exception $e){
             //Best practice: catch all exceptions, log the exception, and display the common error page (or use global exception handling
             //log the exception and display exception view
-            Log::error("Exception: ", array("message" => $e->getMessage()));
+            $this->logger->error("Exception: ", array("message" => $e->getMessage()));
             $data = ['errorMSG' => $e->getMessage()];
             return view('exception')->with($data);
         }
@@ -161,6 +179,7 @@ class GroupController extends Controller
     public function findAllGroups(Request $request)
     {
         try {
+            $this->logger->info("Entering GroupController.findAllGroups()");
             //calls the business service
             $service = new GroupBusinessService();
             
@@ -170,10 +189,12 @@ class GroupController extends Controller
             //fail or succeed return to all groups
             if($success)
             {
+                $this->logger->info("Entering GroupController.findAllGroups with success()");
                 return view("allgroups");
             }
             else
             {
+                $this->logger->info("Entering GroupController.findAllGroups with failure()");
                 return view("allgroups");
             }
             
@@ -181,7 +202,7 @@ class GroupController extends Controller
         catch (Exception $e){
             //Best practice: catch all exceptions, log the exception, and display the common error page (or use global exception handling
             //log the exception and display exception view
-            Log::error("Exception: ", array("message" => $e->getMessage()));
+            $this->logger->error("Exception: ", array("message" => $e->getMessage()));
             $data = ['errorMSG' => $e->getMessage()];
             return view('exception')->with($data);
         }
@@ -202,4 +223,3 @@ class GroupController extends Controller
         $this->validate($request, $rules);
     }
 }
-
