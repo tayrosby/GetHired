91726022fd91bf0eeffe110df433014d9a3bbdfb
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-04-28 00:54:12 -0700

    added logging

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,13 +8,23 @@ namespace App\Http\Controllers;
 use App\Model\UserObjectModel;
 use App\Model\Credentials;
 use App\Services\Business\SecurityService;
+use App\Services\Utility\ILoggerService;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Support\Facades\Log;
 use Exception;
 
 class RegisterController extends Controller
 {
+    protected $logger;
+    
+    /**
+     *
+     * @param ILoggerService $logger
+     */
+    public function __construct(ILoggerService $logger){
+        $this->logger = $logger;
+    }
+    
     /**
      * register() is responsible for registering a user.
      * @param Request $request
@@ -24,48 +34,52 @@ class RegisterController extends Controller
     public function register(Request $request)
     {
         try {
-        //validate the form data(will redirect back to login view if errors)
-        $this->validateForm($request);
-        
-        // Variables are taken from the Register view.
-        $firstName = $request->input('firstname');
-        $lastName = $request->input('lastname');
-        $username = $request->input('username');
-        $password = $request->input('password');
-        $email = $request->input('email');
-        $role = 0;
-        
-        // A user is created using the input
-        $credentials = new Credentials($username, $password);
-        $user = new UserObjectModel($firstName, $lastName, $credentials, $email, $role);
-        
-        //calls the security service
-        $security = new SecurityService();
-        
-        //checks the database to see if the user already exists
-        $duplicate = $security->checkDUP($user);
-        
-        //if there is a duplicate keep the user on the registration page
-        if (!$duplicate) {
-            return view('registrationpage');
-        }
-        //else let the registration process continues
-        else{
-        // saves the result of the registration.
-        $success = $security->register($user);
-        
-        // if register is successful, send the user to the login page
-        if ($success)
-        {
-            return view('loginpage');
-        }
-        // if register is unsuccessful, keep user at the registration page
-        else
-        {
-            return view('registrationpage');
-        }
-        }
-        
+            $this->logger->info("Entering RegisterController.register()");
+            //validate the form data(will redirect back to login view if errors)
+            $this->validateForm($request);
+            
+            // Variables are taken from the Register view.
+            $firstName = $request->input('firstname');
+            $lastName = $request->input('lastname');
+            $username = $request->input('username');
+            $password = $request->input('password');
+            $email = $request->input('email');
+            $role = 0;
+            
+            // A user is created using the input
+            $credentials = new Credentials($username, $password);
+            $user = new UserObjectModel($firstName, $lastName, $credentials, $email, $role);
+            
+            //calls the security service
+            $security = new SecurityService();
+            
+            //checks the database to see if the user already exists
+            $duplicate = $security->checkDUP($user);
+            
+            //if there is a duplicate keep the user on the registration page
+            if (!$duplicate) {
+                $this->logger->info("Exiting RegisterController.register() with failure due to duplicate");
+                return view('registrationpage');
+            }
+            //else let the registration process continues
+            else{
+                // saves the result of the registration.
+                $success = $security->register($user);
+                
+                // if register is successful, send the user to the login page
+                if ($success)
+                {
+                    $this->logger->info("Exiting RegisterController.register() with success");
+                    return view('loginpage');
+                }
+                // if register is unsuccessful, keep user at the registration page
+                else
+                {
+                    $this->logger->info("Exiting RegisterController.register() with failure");
+                    return view('registrationpage');
+                }
+            }
+            
         }
         catch (ValidationException $e1){
             //catch and rethrow the data validation exceptions (so we can catch all others in our next exception catch block)
@@ -74,7 +88,7 @@ class RegisterController extends Controller
         catch (Exception $e){
             //Best practice: catch all exceptions, log the exception, and display the common error page (or use global exception handling
             //log the exception and display exception view
-            Log::error("Exception: ", array("message" => $e->getMessage()));
+            $this->logger->error("Exception: ", array("message" => $e->getMessage()));
             $data = ['errorMSG' => $e->getMessage()];
             return view('exception')->with($data);
         }
@@ -84,7 +98,7 @@ class RegisterController extends Controller
      * validates the data in the form
      * @param Request $request
      */
-     private function validateForm(Request $request){
+    private function validateForm(Request $request){
         //setup data validation rules for login form
         
         $rules = ['firstname' => 'Required | Between:3,15',
@@ -92,7 +106,7 @@ class RegisterController extends Controller
             'email' => 'Required | email',
             'username' => 'Required | Between:3,10',
             'password' => 'Required | Between:3,10 | alpha_num',
-
+            
         ];
         
         //run data validation rules
