7acd0ffd71244f957576f77ddb7c5eadc0fe201f
Taylor Rosby <37849212+tayrosby@users.noreply.github.com>
2019-04-27 19:19:34 -0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,15 +9,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Services\Business\UserBusinessService;
-use Illuminate\Support\Facades\Log;
-use Exception;
-use App\Model\Credentials;
-use App\Model\UserObjectModel;
 use App\Services\Utility\ILoggerService;
+use Exception;
 
 class AdminController extends Controller
 {
- 
     protected $logger;
     
     public function __construct(ILoggerService $logger){
@@ -30,6 +26,7 @@ class AdminController extends Controller
     public function index()
     {
         try {
+        $this->logger->info("Entering AdminController.index()");
         // Calls the business service
         $service = new UserBusinessService();
         // Gets all the users in the database
@@ -37,13 +34,14 @@ class AdminController extends Controller
         // Puts the users in an associative array
         $data = ['users' => $users];
         // Return to the Admin page with the data
+        $this->logger->info("Exiting AdminController.index()");
         return view("adminpage")->with($data);
         } 
     
     catch (Exception $e){
         //Best practice: catch all exceptions, log the exception, and display the common error page (or use global exception handling
         //log the exception and display exception view
-        Log::error("Exception: ", array("message" => $e->getMessage()));
+        $this->logger->error("Exception: ", array("message" => $e->getMessage()));
         $data = ['errorMSG' => $e->getMessage()];
         return ($data);
     }
@@ -57,6 +55,7 @@ class AdminController extends Controller
     public function suspendUser(Request $request)
     {
         try {
+            $this->logger->info("Entering AdminController.suspendUser()");
             // Takes the user's id
             $id = $request->input('ID');
             // Calls the business service
@@ -64,13 +63,14 @@ class AdminController extends Controller
             // suspends the user
             $service->suspendUser($id);
             // redirects to the admin page
+            $this->logger->info("Exiting AdminController.suspendUser()");
             return redirect("/admin");
         }
         
         catch (Exception $e){
             //Best practice: catch all exceptions, log the exception, and display the common error page (or use global exception handling
             //log the exception and display exception view
-            Log::error("Exception: ", array("message" => $e->getMessage()));
+            $this->logger->error("Exception: ", array("message" => $e->getMessage()));
             $data = ['errorMSG' => $e->getMessage()];
             return ($data);
         }
@@ -84,6 +84,7 @@ class AdminController extends Controller
     public function deleteUser(Request $request)
     {
         try {
+        $this->logger->info("Entering AdminController.deleteUser()");
         // Takes the user's id
         $id = $request->input('ID');
         // Calls the business service
@@ -91,13 +92,14 @@ class AdminController extends Controller
         // deletes the user based on their id
         $service->deleteUser($id);
         // redirects to the admin page
+        $this->logger->info("Exiting AdminController.deleteUser()");
         return redirect("/admin");
         }
         
         catch (Exception $e){
             //Best practice: catch all exceptions, log the exception, and display the common error page (or use global exception handling
             //log the exception and display exception view
-            Log::error("Exception: ", array("message" => $e->getMessage()));
+            $this->logger->error("Exception: ", array("message" => $e->getMessage()));
             $data = ['errorMSG' => $e->getMessage()];
             return ($data);
         }
